a92750d1dff16ecf3c6bee66b364a9925f3fbef1
plane000 <playsplane@gmail.com>
2018-10-16 19:14:59 +0100

    Redid structure

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const Logger = require('./logger');
+const Config = require('./config');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+Logger.init();
+Logger.SetLevel(Logger.VERBOSE_LOGS);
+
+let config = new Config('./config/config.json');
+config.load();
+
+// client.on('ready', () => {
+//   console.log(`Logged in as ${client.user.tag}!`);
+// });
+
+// client.on('message', msg => {
+//   if (msg.content === 'ping') {
+//     msg.reply('Pong!');
+//   }
+// });
+
+// client.login('token');
