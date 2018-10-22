b62d9a777c493dd8c95141f2ce9564c12c0e3244
plane000 <playsplane@gmail.com>
2018-10-22 11:24:48 +0100

    Redid Database.query code in database.js

--- src/database/database.js
+++ src/database/database.js
@@ -5,19 +5,15 @@ import {Logger} from '../logger';
 
 export class Database extends BaseDatabase {
     static async exec(query) {
-        let connection = BaseDatabase.Connection;
-        return new Promise((resolve, reject) => {
-            connection
-                .query(query)
-                .then(result => {
-                    Logger.database(JSON.stringify(res, null, 4));
-                    resolve(result[0][0].result);
-                })
-                .catch(err => {
-                    Logger.error('An error occured while querying a database: ' + err);
-                    reject()
-                });
-        });
+        const connection = super.Connection;
+        try {
+            const result = await connection.query(query);
+            Logger.database(JSON.stringify(result, null, 4));
+            return result[0][0].result;
+        }
+        catch (e) {
+            Logger.error(`An error occured while querying a database: ${e}`);
+        }
     }
 }
 
