7e055552fb08dffa55fcd99b370f9640a79b2274
plane000 <playsplane@gmail.com>
2018-10-28 21:57:56 +0000

    Working on getting event handler to pass each object obj, next. next() invoking the next object in the array of callbacks

--- src/events.js
+++ src/events.js
@@ -1,15 +1,30 @@
 import {Logger} from './logger';
 import {Config} from './config';
 
-export class Events {
+export class Events { // extends rate limits
     async init(client) {
         this.client = client;
         this.client.login(Config.Token);
     }
 
     async handleEvents() {
-        this.client.on('ready', () => {this.handleReady()});
-        this.client.on('message', async (message) => {this.handleMessage(message)});
+        this.client.on('ready', async () => {
+            this.handle(undefined, 
+                [this.handleReady, this.anotherCallback]
+            );
+        });
+
+        this.client.on('message', async (message) => {
+            this.handle(message, 
+                [this.handleMessage]
+            );
+        });
+    }
+
+    async handle(obj = [null], callbacks = [undefined]) {
+        // const next = undefined;
+        // if (callbacks[i+1]) const next = callbacks[i+1];
+        // callbacks[i](obj, next);
     }
 
     async handleReady() {
@@ -17,6 +32,10 @@ export class Events {
         this.client.user.setActivity(Config.NowPlaying);
         Logger.info(`Discord client logged in as ${this.client.user.tag}`);
         Logger.ready();
+    }
+
+    async anotherCallback() {
+        Logger.debug(1);
     }
 
     async handleMessage(...args) {
